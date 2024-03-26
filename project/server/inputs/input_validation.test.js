32c52938cc468de7bb40188f451fade2a0024aaa
VinayGaddamGG <vinayrgaddam@gmail.com>
2024-03-25 19:03:28 -0500

    updated testing

--- project/server/inputs/input_validation.test.js
+++ project/server/inputs/input_validation.test.js
@@ -1,15 +1,16 @@
 import {validateUserRegistration} from "./input_validation";
 
 describe('Input Validation', () => {
-    test('Validate User Registration', async () => {
+    test('Valid TestCase', async () => {
       // Test case 1: Valid input
       const validInput = ['username', 'email@example.com', 'Password1!', 'Password1!'];
       await expect(validateUserRegistration(...validInput)).resolves.not.toThrow();
-  
-      // Test case 2: Invalid username (empty)
-      const invalidUsername = ['', 'email@example.com', 'Password1!', 'Password1!'];
-      await expect(validateUserRegistration(...invalidUsername)).rejects.toThrow("Username cannot be empty");
-  
       // Add more test cases for other validation rules
     });
+    test('Invalid TestCase, empty username', async () => {
+        // Test case 2: Valid input
+        const invalidUsername = ['', 'email@example.com', 'Password1!', 'Password1!'];
+        await expect(validateUserRegistration(...invalidUsername)).rejects.toThrow("Username cannot be empty");
+      });
+    
   });
